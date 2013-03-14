5465d2c0cd42d8319e0c7d6d81d53610fa647cab
alongken2005 <360586201@qq.com>
2013-03-15 00:08:19 +0800

    更新

--- Classes/start.cpp
+++ Classes/start.cpp
@@ -10,10 +10,41 @@ CCScene* start::scene()
 		scene = CCScene::create();
 		CC_BREAK_IF(!scene);
 
-		start* startbg = start::create();
-		CC_BREAK_IF(!startbg);
+		CCSize size = CCDirector::sharedDirector()->getWinSize();
 
+		start* startbg = start::create();
+		CCSprite* bgSprite = CCSprite::create("MainScreen/cover.jpg");
+		bgSprite->setPosition(ccp(size.width/2, size.height/2));
+		startbg->addChild(bgSprite);
+
+		CCSprite* logo = CCSprite::create("MainScreen/cn_logo.png");
+		logo->setPosition(ccp(size.width/2, size.height/2));
+
+		CCSize shsize;
+		shsize.width = 5;
+		shsize.height = 6;
+		CCActionInterval* actionMoveTo = CCMoveTo::create(1, ccp(size.width/2 - 300, size.height/2 + 200));
+		CCActionInterval* actionScaleTo1 = CCScaleTo::create(0, 0.6f);
+		CCActionInterval* actionScaleTo2 = CCScaleTo::create(0.5, 1.0f);
+		
+		logo->runAction(actionMoveTo);
+		logo->runAction(CCSequence::create(actionScaleTo1, actionScaleTo2, NULL));
+
+		startbg->addChild(logo);
+
+
+		CCSprite* enter1 = CCSprite::create("MainScreen/button_disabled.png");
+		enter1->setPosition(ccp(size.width/2, 80));
+		CCSprite* enter2 = CCSprite::create("cn_/ButtonWord/cn_buttonword_10.png");
+		enter2->setPosition(ccp(size.width/2, 80));
+		enter2->setScale(0.4f);
+		startbg->addChild(enter1);
+		startbg->addChild(enter2);
+		//CCActionInterval* shaky = CCShaky3D::create(1, shsize, 8, 0);
+		//CCFiniteTimeAction*  action = CCSequence::create(CCDelayTime::create(0.5), shaky, NULL);
+		//startbg->runAction(action);
 		scene->addChild(startbg);
+
 	} while (0);
 
 	return scene;
@@ -25,11 +56,7 @@ bool start::init()
 	do 
 	{
 		CC_BREAK_IF(!CCLayer::init());
-		CCSize size = CCDirector::sharedDirector()->getWinSize();
 
-		CCSprite* bgSprite = CCSprite::create("MainScreen/cover.jpg");
-		bgSprite->setPosition(ccp(size.width/2, size.height/2));
-		this->addChild(bgSprite);
 		bRet = true;
 	} while (0);
 
